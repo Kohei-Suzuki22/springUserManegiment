16a90d768c8726e42d0c2752213a6324e380e539
kohei <bhkbb0551@gmail.com>
2021-11-28 23:12:13 +0900

    create Controllers

---
+++ src/main/java/com/user/manegiment/basis/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.user.manegiment.basis.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/login")
+	public String getLogin() {
+		return "/login/login";
+	}
+}
--- src/main/java/com/user/manegiment/basis/controller/UserController.java
+++ src/main/java/com/user/manegiment/basis/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.user.manegiment.basis.service.UserService;
@@ -24,6 +25,11 @@ public class UserController {
 		model.addAttribute("gender", gender);
 		return "/user/signup";
 		
+	}
+	
+	@PostMapping("/signup")
+	public String postSignup() {
+		return "redirect:/login";
 	}
 	
 }
