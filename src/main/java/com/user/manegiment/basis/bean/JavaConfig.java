7f9a2db646dc3cf2947b143bf21c3010abb6608b
kohei <bhkbb0551@gmail.com>
2021-12-11 19:10:14 +0900

    add messages.properties

---
+++ src/main/java/com/user/manegiment/basis/bean/JavaConfig.java
@@ -0,0 +1,21 @@
+package com.user.manegiment.basis.bean;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@Configuration
+public class JavaConfig {
+
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		
+		messageSource.setBasename("messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		
+		return messageSource;
+		
+	}
+}
