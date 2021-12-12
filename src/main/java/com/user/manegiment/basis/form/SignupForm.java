75cdc3842a9ed121ce03ff80bebd6678b02a5712
kohei <bhkbb0551@gmail.com>
2021-12-12 18:11:33 +0900

    add validation to SignupForm

--- src/main/java/com/user/manegiment/basis/form/SignupForm.java
+++ src/main/java/com/user/manegiment/basis/form/SignupForm.java
@@ -2,6 +2,13 @@ package com.user.manegiment.basis.form;
 
 import java.util.Date;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
@@ -9,12 +16,26 @@ import lombok.Data;
 @Data
 public class SignupForm {
 	
+	@NotBlank
+	@Email
 	private String userId;
+
+	@NotBlank
+	@Length(min=4, max=50)
+	@Pattern(regexp = "^[a-zA-Z0-9]+$")
 	private String password;
+	
+	@NotBlank
 	private String userName;
+	
 	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	@NotNull
 	private Date birthday;
+
+	@Range(min=0, max=100)
 	private Integer age;
+	
+	@NotNull
 	private Integer gender;
 	
 	
